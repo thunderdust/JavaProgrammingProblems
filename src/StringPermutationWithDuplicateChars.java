bd4a91778a91177b762edd065d08a5e466ffc31a
thunderdust <liuweiran.nus@gmail.com>
2015-11-03 17:03:24 +0800

    Add String permutation problems (2 sub-cases)

---
+++ src/StringPermutationWithDuplicateChars.java
@@ -0,0 +1,7 @@
+/* Given a string, permute all the combinations of the characters from the string. 
+ * It's possible to have repetitive characters in the string, for example: "AAC" */
+
+
+public class StringPermutationWithDuplicateChars {
+
+}
---
+++ src/StringPermutationWithoutDuplicateChars.java
@@ -0,0 +1,29 @@
+/* Given a string, permute all the combinations of the characters from the string. 
+ * Assuming there is no duplicated characters in the string. */
+
+public class StringPermutationWithoutDuplicateChars {
+
+	public static void main(String args[]) {
+		permutateStringRecursively("", "ABCD");
+
+	}
+
+	// Set prefix as "" as the beginning
+	public static void permutateStringRecursively(String prefix, String sample) {
+
+		int length = sample.length();
+		if (length == 0) {
+			System.out.println(prefix);
+		} 
+		else {
+			for (int i = 0; i < length; i++) {
+				// append every single char from sample to current prefix as the
+				// new prefix
+				permutateStringRecursively(prefix + sample.charAt(i),
+						sample.substring(0, i) + sample.substring(i + 1, length));
+			}
+		}
+	}
+	
+	//TO-DO: None-recursive method
+}
