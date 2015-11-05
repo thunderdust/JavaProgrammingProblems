eb0f42682d4a7be4851d1c4b7576ff5cc722fbe6
thunderdust <liuweiran.nus@gmail.com>
2015-11-05 11:45:52 +0800

    Add 3 implementations for rotate array by given offset

--- src/ArrayProlems/ArrayRotate.java
+++ src/ArrayProlems/ArrayRotate.java
@@ -6,38 +6,112 @@ package ArrayProlems;
 public class ArrayRotate {
 
 	public static void main(String args[]) {
-		int[] array_original = {1,2,3,4,5,6,7,8};
-		int[] result = new int[array_original.length];
-		result = rotateArray1(array_original, 5);
-		printElements(result); 
-
+		int[] array_original = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int steps = 2;
+		printElements(bubbleRotate(array_original, steps));
+		reset(array_original);
+		printElements(rotateWithOffset(array_original, steps));
+		reset(array_original);
+		printElements(reversalRotate(array_original, steps));
 	}
 
-	public static int[] rotateArray1(int[] array, int step) {
+	private static void reset(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i + 1;
+		}
+	}
+
+	// Like a bubble sort. Time O(n*k), space O(1)
+	public static int[] bubbleRotate(int[] array, int step) {
+
+		if (array.length == 0 || array.length == 1 || array != null && step == 0) {
+			return array;
+		}
+		if (array == null || step < 0) {
+			throw new IllegalArgumentException("Illegal argument!");
+		}
 
 		if (step > array.length) {
 			step = step % array.length;
 		}
-		int[] result = new int[array.length];
 
 		for (int i = 0; i < step; i++) {
-			result[i] = array[array.length-step+i];
+			for (int j = array.length - 1; j > 0; j--) {
+				int temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+			}
 		}
-		int j = 0;
-		for (int i = step; i < array.length; i++) {
-			result[i] = array[j];
-			j++;
-		}
-		return result;
+		return array;
 	}
-	
-	public static void printElements(int[] array){
-		
+
+	public static int[] rotateWithOffset(int[] array, int step) {
+
+		if (array.length == 0 || array.length == 1 || array != null && step == 0) {
+			return array;
+		}
+		if (array == null || step < 0) {
+			throw new IllegalArgumentException("Illegal argument!");
+		}
+
+		if (step > array.length) {
+			step = step % array.length;
+		}
+
+		int size = array.length;
+		int[] afterRotate = new int[size];
+		for (int i = 0; i < size; i++) {
+			afterRotate[(i + step) % size] = array[i];
+		}
+		return afterRotate;
+	}
+
+	// Time O(n), space O(1)
+	public static int[] reversalRotate(int[] array, int step) {
+
+		if (array.length == 0 || array.length == 1 || array != null && step == 0) {
+			return array;
+		}
+		if (array == null || step < 0) {
+			throw new IllegalArgumentException("Illegal argument!");
+		}
+
+		if (step > array.length) {
+			step = step % array.length;
+		}
+
+		reverse(array, 0, array.length - 1);
+		reverse(array, 0, step - 1);
+		reverse(array, step, array.length - 1);
+		/*
+		 * IF put reverse whole array at last, the rotate direction will be
+		 * opposite (anti-clockwise); Can deal with NEGATIVE reverse steps
+		 */
+		return array;
+	}
+
+	public static void reverse(int[] array, int start, int end) {
+		if (array == null || array.length == 1) {
+			return;
+		} else {
+			while (start < end) {
+				int temp = array[start];
+				array[start] = array[end];
+				array[end] = temp;
+				start++;
+				end--;
+			}
+		}
+	}
+
+	public static void printElements(int[] array) {
+
 		System.out.println("Result:");
 		int index = 0;
-		while (index < array.length){
-			System.out.print(array[index]+" ");
+		while (index < array.length) {
+			System.out.print(array[index] + " ");
 			index++;
 		}
+		System.out.println();
 	}
 }
