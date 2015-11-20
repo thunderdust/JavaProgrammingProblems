c2921b1d5e7292a218e4c0a2edf5ffb6756d5f44
thunderdust <liuweiran.nus@gmail.com>
2015-11-20 15:08:28 +0800

    Solve Median of 2 sorted array problem

--- src/ArrayProlems/MedianOfTwoSortedArrays.java
+++ src/ArrayProlems/MedianOfTwoSortedArrays.java
@@ -10,26 +10,50 @@ public class MedianOfTwoSortedArrays {
 
 		int[] A = { 1 };
 		int[] B = { 1 };
-		System.out.println("Median of sorted array A and B is: " + getMedian(A, B));
+		int totalSize = A.length + B.length;
+		double median;
+		// Find median based on odd/even
+		if (totalSize % 2 == 1) {
+			median = findKth(A, A.length, B, B.length, totalSize / 2 + 1);
+		} else {
+			median = (findKth(A, A.length, B, B.length, totalSize / 2 + 1)
+					+ findKth(A, A.length, B, B.length, totalSize / 2)) / 2.0;
+		}
+		System.out.println("Median of sorted array A and B is: " + median);
 	}
 
-	// Find the kth element from 2 sorted array
-	public static int findKth(int A[], int B[], int k, int aStart, int aEnd, int bStart, int bEnd) {
+	public static double findKth(int a[], int m, int b[], int n, int k) {
+		// always assume that m<= n
+		if (m > n)
+			return findKth(b, n, a, m, k);
+		if (m == 0)
+			return b[k - 1];
+		if (k == 1)
+			return Math.min(a[0], b[0]);
+		// divide K into 2 parts
+		int numberA = Math.min(k / 2, m), numberB = k - numberA;
 
-		int aLen = aEnd - aStart + 1;
-		int bLen = bEnd - bStart + 1;
-		// Special cases
-		if (aLen == 0) {
-			return B[bStart + k];
+		// first numberA number of elements from array a is less than K, remove
+		// them from the search
+		if (a[numberA - 1] < b[numberB - 1]) {
+			int[] cropedA = arrayCrop(a, numberA);
+			return findKth(cropedA, m - numberA, b, n, k - numberA);
 		}
-		if (bLen == 0) {
-			return A[aStart + k];
-		}
-		if (k == 0) {
-			return Math.max(A[aStart], B[bStart]);
-		}
-		
-		int aMid = aLen*k / (aLen + bLen);
+		// first numberB of elements from array b is less than K, remove them
+		// from the search
+		else if (a[numberA - 1] > b[numberB - 1]) {
+			int[] cropedB = arrayCrop(b, numberB);
+			return findKth(a, m, cropedB, n - numberB, k - numberB);
+		} else
+			return a[numberA - 1];
+	}
 
+	private static int[] arrayCrop(int originalArray[], int cropSize) {
+		int newSize = originalArray.length - cropSize;
+		int[] newArray = new int[newSize];
+		for (int i = 0; i < newSize; i++) {
+			newArray[i] = originalArray[i + cropSize];
+		}
+		return newArray;
 	}
 }
